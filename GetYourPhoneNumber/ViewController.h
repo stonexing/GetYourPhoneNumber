a0a79b8c5e81e269504587c6a3acaadeabc6ba4a
stonexing <stonexing5@gmail.com>
2013-09-10 21:35:56 +0800

    Initial Commit

---
+++ GetYourPhoneNumber/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GetYourPhoneNumber
+//
+//  Created by Xingyan on 13-9-10.
+//  Copyright (c) 2013年 Xingyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GetYourPhoneNumber/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GetYourPhoneNumber
+//
+//  Created by Xingyan on 13-9-10.
+//  Copyright (c) 2013年 Xingyan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
